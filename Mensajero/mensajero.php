fe2b8bdae26a68f298e605e2e83c077af7eb4491
Maugara8 <160251601+Maugara8@users.noreply.github.com>
2024-05-01 21:10:06 -0600

    Add files via upload

--- Mensajero/mensajero.php
+++ Mensajero/mensajero.php
@@ -8,7 +8,7 @@ if(isset($_POST['submit']))
 	$correo = $_POST['email'];
 	$mensaje = $_POST['mensaje'];
 	$headers = "MIME-Version: 1.0\r\n"; 
-	$headers .= "Content-type: text/html; charset=utf8\r\n";
+	$headers = "Content-type: text/html; charset=utf8\r\n";
 	$cuerpo = " Enviado por: $nombre\n E-Mail: $correo\n Mensaje: $mensaje\n";
 
 	$bool = mail($para,$asunto,$cuerpo,$headers);
