fd433856433d21e12f02e1c5ab6d8b80213f5a97
Maugara8 <160251601+Maugara8@users.noreply.github.com>
2024-05-02 00:27:49 -0600

    Add files via upload

--- mensajero.php
+++ mensajero.php
@@ -1,17 +1,22 @@
 <?php
 if(isset($_POST['submit'])) 
 {
-
-	$para = "184798@upslp.edu.mx"; 
-	$asunto = "Enviando informacion";
 	$nombre = $_POST['nombre'];
 	$correo = $_POST['email'];
 	$mensaje = $_POST['mensaje'];
-	$headers = "MIME-Version: 1.0\r\n"; 
-	$headers .= "Content-type: text/html; charset=utf8\r\n";
+
+	$para = "184798@upslp.edu.mx"; 
+	$asunto = "Enviando informacion";
+
+	$contenido = "nombre: $nombre /n "
+	$contenido .= "correo: $correo /n "
+	$contenido .= "mensaje: $mensaje /n "
+
+	/*$headers = "MIME-Version: 1.0\r\n"; 
+	$headers .= "Content-type: text/html; charset=utf8\r\n";*/
 	$cuerpo = " Enviado por: $nombre\n E-Mail: $correo\n Mensaje: $mensaje\n";
 
-	mail($para,$asunto,$cuerpo,$headers);
+	$mail=mail($para,$asunto,$cuerpo,$contenido);
 } 
 else
 {
