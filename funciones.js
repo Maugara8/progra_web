0124db470ffb78d913ceea1f4efb9d5abc8817e4
Maugara8 <160251601+Maugara8@users.noreply.github.com>
2024-05-23 08:56:46 -0600

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -1,13 +1,13 @@
-function saber(cadena){
-    let cadena=document.getElemcentById("cadena").value;
-    if(cadena===cadena.tolowerCase()){
-        let r="la cadena"+ cadena+"esta formada por minusculas";
-        document.getElementById("parrafo").innerHTML=r;
-    }else if (cadena===cadena.toUpperCase()){
-        let r="la cadena"+ cadena+"esta formada por mayusculas";
-        document.getElementById("parrafo").innerHTML=r;
-    }else {
-        let r="la cadena tien de los dos";
-        document.getElementById("parrafo").innerHTML=r;
-    }
+function evalua() {
+  let cadena = document.getElementById("cadena").value;
+  if (cadena === cadena.toLowerCase()) {
+    let r = "La cadena " + cadena + " está formada por minúsculas.";
+    document.getElementById("parrafo").innerHTML = r;
+  } else if (cadena === cadena.toUpperCase()) {
+    let r = "La cadena " + cadena + " está formada por mayúsculas.";
+    document.getElementById("parrafo").innerHTML = r;
+  } else {
+    let r = "La cadena tiene de los dos.";
+    document.getElementById("parrafo").innerHTML = r;
+  }
 }
